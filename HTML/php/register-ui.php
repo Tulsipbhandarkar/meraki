321eb0d8b9428eb5465f609d8e1e52b40b88d78e
Tulsipbhandarkar <53314877+Tulsipbhandarkar@users.noreply.github.com>
2021-06-26 20:37:16 +0530

    Add files via upload

---
+++ HTML/php/register-ui.php
@@ -0,0 +1,159 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Validate username
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter a username.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = trim($_POST["username"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $username_err = "This username is already taken.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Validate password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+        
+        // Prepare an insert statement
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            
+            // Set parameters
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Redirect to login page
+                header("location: login.php");
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <title>Visual Admin - Login</title>
+        <meta name="description" content="">
+        <meta name="author" content="templatemo">
+        <!--
+        Visual Admin Template
+        https://templatemo.com/tm-455-visual-admin
+        -->
+	    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,700' rel='stylesheet' type='text/css'>
+	    <link href="css/font-awesome.min.css" rel="stylesheet">
+	    <link href="css/bootstrap.min.css" rel="stylesheet">
+	    <link href="css/templatemo-style.css" rel="stylesheet">
+
+	    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+	</head>
+
+	<body class="light-gray-bg">
+		<div class="templatemo-content-widget templatemo-login-widget white-bg">
+			<header class="text-center">
+	          <div class="square"></div>
+	          <h1>Registration</h1>
+	        </header>
+	        <form action="index.html" class="templatemo-login-form">
+	        	<div class="form-group">
+					<h3 class="margin-bottom-10" style="font-size: 15px">Username</h3>
+	        		<div class="input-group width-100">
+		              	<input type="text" class="form-control pt-8" placeholder="user">
+		          	</div>
+	        	</div>
+	        	<div class="form-group">
+					<h3 class="margin-bottom-10" style="font-size: 15px">Email</h3>
+	        		<div class="input-group width-100">
+		              	<input type="text" class="form-control" placeholder="user@gmail.com">
+		          	</div>
+	        	</div>
+				<div class="form-group">
+					<h3 class="margin-bottom-10" style="font-size: 15px">Password</h3>
+	        		<div class="input-group width-100">
+		              	<input type="password" class="form-control" placeholder="******">
+		          	</div>
+	        	</div>
+				<div class="form-group">
+					<h3 class="margin-bottom-10" style="font-size: 15px">Confirm Password</h3>
+	        		<div class="input-group width-100">
+		              	<input type="password" class="form-control" placeholder="******">
+		          	</div>
+	        	</div>
+				<div class="form-group">
+					<button type="submit" class="templatemo-blue-button width-100">Register</button>
+				</div>
+	        </form>
+	</body>
+</html>
